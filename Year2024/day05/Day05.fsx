3f91cc21a2e142c299db823d94bb61d988592bab
markusbrammer <62746606+markusbrammer@users.noreply.github.com>
2024-11-10 12:54:20 +0100

    Setup for 2024

---
+++ Year2024/day05/Day05.fsx
@@ -0,0 +1,21 @@
+#if ! INTERACTIVE 
+module Year2024.Day05
+#endif
+
+open System.IO
+
+let inputPath = $"{__SOURCE_DIRECTORY__}/input.txt"
+let examplePath = $"{__SOURCE_DIRECTORY__}/example.txt"
+
+let part1 () = "NOT IMPLEMENTED"
+    
+#if INTERACTIVE
+part1 ()
+#endif
+
+let part2 () = "NOT IMPLEMENTED"
+
+#if INTERACTIVE
+part2 ()
+#endif
+
---
+++ Year2024/day14/Day14.fsx
@@ -0,0 +1,22 @@
+#if ! INTERACTIVE 
+module Year2024.Day14
+#endif
+
+open System.IO
+
+let inputPath = $"{__SOURCE_DIRECTORY__}/input.txt"
+let examplePath = $"{__SOURCE_DIRECTORY__}/example.txt"
+
+let part1 () =
+    File.ReadAllText inputPath
+    
+#if INTERACTIVE
+part1 ()
+#endif
+
+let part2 () = printfn "NOT IMPLEMENTED"
+
+#if INTERACTIVE
+part2 ()
+#endif
+
